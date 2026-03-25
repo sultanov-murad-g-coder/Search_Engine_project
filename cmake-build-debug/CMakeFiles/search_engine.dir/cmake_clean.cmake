07de5a7757229b800f74e247a3688571273ab042
SultanovMurad <sultanov.murad.g@gmail.com>
2026-03-25 20:17:49 +0300

    delete full code in main

---
+++ cmake-build-debug/CMakeFiles/search_engine.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_engine.dir/src/converter_json.cpp.obj"
+  "CMakeFiles/search_engine.dir/src/converter_json.cpp.obj.d"
+  "CMakeFiles/search_engine.dir/src/engine.cpp.obj"
+  "CMakeFiles/search_engine.dir/src/engine.cpp.obj.d"
+  "CMakeFiles/search_engine.dir/src/entry.cpp.obj"
+  "CMakeFiles/search_engine.dir/src/entry.cpp.obj.d"
+  "CMakeFiles/search_engine.dir/src/inverter_index.cpp.obj"
+  "CMakeFiles/search_engine.dir/src/inverter_index.cpp.obj.d"
+  "CMakeFiles/search_engine.dir/src/relative_index.cpp.obj"
+  "CMakeFiles/search_engine.dir/src/relative_index.cpp.obj.d"
+  "CMakeFiles/search_engine.dir/src/search_server.cpp.obj"
+  "CMakeFiles/search_engine.dir/src/search_server.cpp.obj.d"
+  "libsearch_engine.dll.a"
+  "search_engine.exe"
+  "search_engine.exe.manifest"
+  "search_engine.pdb"
+  "search_engine[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/search_engine.dir/engine.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/search_engine.dir/engine.cpp.obj: \
+ C:\projectClion\search_engine\engine.cpp
