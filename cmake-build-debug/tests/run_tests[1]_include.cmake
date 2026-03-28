5b631e2836525ffc4bd900acca89759b4f8ed25d
SultanovMurad <sultanov.murad.g@gmail.com>
2026-03-28 23:40:19 +0300

    Fix: remove build directories from tracking, update gitignore

--- cmake-build-debug/tests/run_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/projectClion/search_engine/cmake-build-debug/tests/run_tests[1]_tests.cmake")
-  include("C:/projectClion/search_engine/cmake-build-debug/tests/run_tests[1]_tests.cmake")
-else()
-  add_test(run_tests_NOT_BUILT run_tests_NOT_BUILT)
-endif()
